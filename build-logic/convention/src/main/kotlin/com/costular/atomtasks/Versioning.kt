a1134f1ca0a20b9927dcee7cb7f184f617140d3f
Diego Francisco Concepción <costular@gmail.com>
2026-05-08 19:48:00 +0100

    Bump app version

--- build-logic/convention/src/main/kotlin/com/costular/atomtasks/Versioning.kt
+++ build-logic/convention/src/main/kotlin/com/costular/atomtasks/Versioning.kt
@@ -1,6 +1,6 @@
 package com.costular.atomtasks
 
 object Versioning {
-    const val VersionCode = 30
-    const val VersionName = "2.10.0"
+    const val VersionCode = 31
+    const val VersionName = "2.11.3"
 }
